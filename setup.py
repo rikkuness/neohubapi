a3eb2699c80b2c470292ff7a9c49399fdaad1b31
Andrius Štikonas <andrius@stikonas.eu>
2020-12-27 18:23:05 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2020 Andrius Štikonas <andrius@stikonas.eu>
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+setup(
+    name = "neohubapi",
+    version = "0.1",
+    description = "Async library to communicate with Heatmiser NeoHub 2 API.",
+    url = "https://gitlab.com/neohubapi/neohubapi/",
+    author = "Andrius Štikonas",
+    author_email = "andrius@stikonas.eu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    packages = ['neohubapi'],
+    install_requires = ['neohubapi'],
+    keywords = ['neohub', 'heatmiser'],
+)
